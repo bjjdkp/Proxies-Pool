c934f01722b9cd0397f3e3997366b1a31f1d394e
bjjdkp <bjjdkp@gmail.com>
2019-07-01 14:09:00 +0800

    add mongo client

---
+++ db/mongo.py
@@ -0,0 +1,33 @@
+# --*-- coding:utf-8 --*--
+
+import pymongo
+from config import *
+
+
+class Mongo(object):
+
+    def __init__(self):
+        self.mongo_user = MONGO_USR
+        self.mongo_pwd = MONGO_PWD
+        self.host = MONGO_HOST
+        self.db_name = MONGO_DB_SOURCE
+        self.client = pymongo.MongoClient(self.host)
+        self.db = self.client[self.db_name]
+        self.db.authenticate(self.mongo_user, self.mongo_pwd,
+                             mechanism='SCRAM-SHA-1')
+
+    def get_conn(self, collection_name):
+        return self.db[collection_name]
+
+    def save_data(self, collection_name, data):
+        self.db[collection_name].insert(data)
+
+    def init_index(self, collection_name):
+        index1 = pymongo.IndexModel([("host", 1)], unique=True)
+        index2 = pymongo.IndexModel([("host_status", 1)])
+        self.db[collection_name].create_indexes([index1, index2])
+
+
+
+
+
