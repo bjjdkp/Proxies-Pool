7980a2134c26adf27a766c75d51b22df6cb91391
bjjdkp <bjjdkp@gmail.com>
2019-08-12 11:01:39 +0800

    del redis config

--- settings.py
+++ settings.py
@@ -10,10 +10,6 @@ MONGO_COLLECTION_HTTPS = "https"
 MONGO_USR = "test"
 MONGO_PWD = "test"
 
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_PASSWORD = None
-
 # score for proxy
 MAX_SCORE = 100
 MIN_SCORE = 0
